51718650b8f9118cfc7e30622d036c001ccf6911
Nastia Bzova <bzovanastia@gmail.com>
2025-09-02 15:31:05 +0200

    HeroSection

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+export default function HeroSection() {
+  return (
+    <div className="text-center my-4 px-4">
+      <h1 className="mb-3"><strong>Cook deliciously from the ingredients you have on hand</strong></h1>
+      <p className="lead">
+        It is enough to choose only 1 product, and we will find a recipes for you!
+      </p>
+    </div>
+  );
+}
