7697a02089332dfd4e1e19330efc7d8535dd69c9
Nastia Bzova <bzovanastia@gmail.com>
2025-09-02 15:19:28 +0200

    Header link and brand logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,5 +2,22 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.gif";
 
 export default function Header() {
-  
+  return (
+    <Navbar bg="light" expand="lg" className="mb-4">
+      <Container className="justify-content-between">
+        <Nav>
+          <Nav.Link href="#">Favourites</Nav.Link>
+        </Nav>
+        <div className="d-flex align-items-center">
+          <img src={logo} className="img-fluid me-2" style={{ height: '40px' }} alt="Logo" />
+          <Navbar.Brand href="#home" className="fw-bold m-0">
+            One Ingredient Chef
+          </Navbar.Brand>
+        </div>
+        <Nav>
+          <Nav.Link href="#">New recipe</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
