f21c4b0ed9e4449b4a13a32a72eb1aa53ac4c4bf
Nastia Bzova <bzovanastia@gmail.com>
2025-09-02 18:46:37 +0200

    import SearchBar (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import SearchBar from "./components/SearchBar";
 import RecipeCard from "./components/RecipeCard";
 import RecipeDetailScreen from "./components/RecipeDetailScreen";
 
