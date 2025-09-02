e22eba7b18895d8daf5af6e8cf99bcec5b579718
Nastia Bzova <bzovanastia@gmail.com>
2025-09-02 18:05:19 +0200

    import components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import RecipeCard from "./components/RecipeCard";
+import RecipeDetailScreen from "./components/RecipeDetailScreen";
 
 export default function App() {
   return (
