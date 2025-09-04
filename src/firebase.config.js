5e9bd1e798504bc44ebc4a241a02704a07b4da2b
Nastia Bzova <bzovanastia@gmail.com>
2025-09-04 21:21:08 +0200

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,34 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDUrzac8LmpwU1X1SDEUcctsqwEA5MZnYU",
   authDomain: "one-ingredient-chef.firebaseapp.com",
   projectId: "one-ingredient-chef",
   storageBucket: "one-ingredient-chef.firebasestorage.app",
   messagingSenderId: "675761069172",
-  appId: "1:675761069172:web:bdb07cfa8d046959d88521",
-  measurementId: "G-CJTH0N494Q"
+  appId: "1:675761069172:web:32ea84be667c6529d88521",
+  measurementId: "G-G1FF4Y76VX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Firestore
 export const db = getFirestore(app);
-
-// Storage (images)
-export const storage = getStorage(app);
-
-// Auth
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
+// Auth login/logout 
 export const loginWithGoogle = () => signInWithPopup(auth, provider);
 export const logout = () => signOut(auth);
