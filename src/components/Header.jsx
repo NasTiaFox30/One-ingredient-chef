51f23767d5393cb719a7c67c591cbf148f0426e8
Nastia Bzova <bzovanastia@gmail.com>
2025-09-04 21:54:14 +0200

    import firebase.config (Header.jsx)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import logo from "../assets/logo.gif";
 
-export default function Header({ onShowFavourites }) {
+import { loginWithGoogle, logout } from "../firebase.config";
+
+export default function Header({ user, onShowFavourites }) {
   return (
     <Navbar bg="light" expand="lg" className="mb-4">
       <Container className="justify-content-between">
