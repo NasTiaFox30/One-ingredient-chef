49af6e2dd03345f7b34fa2097cc29a0dbcb6b805
Nastia Bzova <bzovanastia@gmail.com>
2025-09-04 21:20:53 +0200

    deleted services (not working)

--- src/services/storage.js
+++
@@ -1,9 +0,0 @@
-import { storage } from "../firebase.config";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
-export const uploadImage = async (file) => {
-  const storageRef = ref(storage, `recipes/${Date.now()}-${file.name}`);
-  await uploadBytes(storageRef, file);
-  const url = await getDownloadURL(storageRef);
-  return url;
-};
