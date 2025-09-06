abe23b5d10364017f6300a6bb9bd633658ef45a0
Nastia Bzova <bzovanastia@gmail.com>
2025-09-06 16:28:55 +0200

    added <motion.div to RecipeCard componte again

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,6 +3,12 @@ import { motion } from "framer-motion";
 
 export default function RecipeCard({ recipe, onShow }) {
   return (
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
       <Card style={{ width: "18rem" }} className="mb-4 shadow-sm">
         <Card.Img variant="top" src={recipe.image} />
         <Card.Body>
@@ -11,5 +17,6 @@ export default function RecipeCard({ recipe, onShow }) {
           <Button variant="outline-success" onClick={() => onShow(recipe)}>Lets cook!</Button>
         </Card.Body>
       </Card>
+    </motion.div>
   );
 }
