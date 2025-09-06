d1f3affe441a6918b4be6e4907660d7cb0338dbc
Nastia Bzova <bzovanastia@gmail.com>
2025-09-06 21:11:12 +0200

    added component Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function footer() { 
+    return (
+        <footer className="mt-4 fst-italic text-center" style={{color: "gray"}}>
+            <p>Created by: Anastasiia Bzova &copy 2025</p>
+        </footer>
+    );
+}
