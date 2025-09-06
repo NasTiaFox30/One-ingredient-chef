5445ed879393c065788a509706c36627a87f3b86
Nastia Bzova <bzovanastia@gmail.com>
2025-09-07 00:32:39 +0200

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUrzac8LmpwU1X1SDEUcctsqwEA5MZnYU",
-  authDomain: "one-ingredient-chef.firebaseapp.com",
-  projectId: "one-ingredient-chef",
-  storageBucket: "one-ingredient-chef.firebasestorage.app",
-  messagingSenderId: "675761069172",
-  appId: "1:675761069172:web:32ea84be667c6529d88521",
-  measurementId: "G-G1FF4Y76VX"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
