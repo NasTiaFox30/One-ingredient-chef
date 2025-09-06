c3dee66a5453cf8e0c02b2957a25edbd5b15ff7d
Nastia Bzova <bzovanastia@gmail.com>
2025-09-06 15:57:01 +0200

    added wxample animation to RecipeCard

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,13 +3,21 @@ import { motion } from "framer-motion";
 
 export default function RecipeCard({ recipe, onShow }) {
   return (
-    <Card style={{ width: "18rem" }} className="mb-4 shadow-sm">
-      <Card.Img variant="top" src={recipe.image} />
-      <Card.Body>
-        <Card.Title>{recipe.title}</Card.Title>
-        <Card.Text>⏱ {recipe.time} | ⚡ {recipe.difficulty}</Card.Text>
-        <Button variant="outline-success" onClick={() => onShow(recipe)}>Lets cook!</Button>
-      </Card.Body>
-    </Card>
+    <motion.div
+      className=""
+      initial={{ opacity: 0, y: 100 }}     
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 50 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }} 
+    >
+      <Card style={{ width: "18rem" }} className="mb-4 shadow-sm">
+        <Card.Img variant="top" src={recipe.image} />
+        <Card.Body>
+          <Card.Title>{recipe.title}</Card.Title>
+          <Card.Text>⏱ {recipe.time} | ⚡ {recipe.difficulty}</Card.Text>
+          <Button variant="outline-success" onClick={() => onShow(recipe)}>Lets cook!</Button>
+        </Card.Body>
+      </Card>
+    </motion.div>
   );
 }
